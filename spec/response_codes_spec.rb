1bbd1f5562a4854f1d516cc51c5901f49488c068
Robert Schultheis <rschultheis@github.com>
2017-07-20 14:41:26 -0700

    example showing status code checking

---
+++ spec/response_codes_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "Respose codes for Open Weather Map" do
+  it "should return 200 for a successful query" do
+    weather_for 'San Francisco, CA'
+    expect(owm.last_response_status).to equal(200)
+  end
+
+  it "should return 404 for an unsuccessful query" do
+    expect { weather_for 'xyxyxyxyx' }.to raise_error { |err|
+        expect(err).to be_a(Hatt::RequestException)
+        expect(err.code).to equal(404)
+      }
+  end
+end
